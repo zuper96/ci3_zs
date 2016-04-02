c24ce9d2e25afb6a46c45368edd5a66f2b8dcadf
zuper96 <a_zupri96@yahoo.com>
2016-04-03 04:15:31 +0700

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,7 +68,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Selamat datang di CodeIgniter!</h1>
+	<h1>Selamat datang di CodeIgniter!Testing dari git</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
